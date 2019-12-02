f54a438684cfdb7d0a94acd658f2beac0483613b
Robert Newman <bobs-n@hotmail.com>
2019-12-02 13:29:10 +0000

    Set up project and apps

---
+++ accounts/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def signup(request):
+    return render(request, 'accounts/signup.html')
+
+def login(request):
+    return render(request, 'accounts/login.html')
+
+def logout(request):
+    return HttpResponse('LOGOUT ROUTE')
